f2161777d764c6136f68ae1a62c66973198119dd
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-05-29 15:34:57 +0500

    first commit

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 // Question 3: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
-// let whitespaces_string : string = "\tHello World\n";
-// console.log(`String with whitespaces: ${whitespaces_string}`)
-// let trimmed_whitespaces = whitespaces_string.trim();
-// console.log(`String without whitespaces: ${trimmed_whitespaces}`)
+let whitespaces_string : string = "\tHello World\n";
+console.log(`String with whitespaces: ${whitespaces_string}`)
+let trimmed_whitespaces = whitespaces_string.trim();
+console.log(`String without whitespaces: ${trimmed_whitespaces}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -21,15 +34,15 @@ console.log(12 - 4);
 
 // Question 10: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does.
 
-// I Used previous question to answer this question. 
-// In this line I made a variable called whitespaces_string
-let whitespaces_string : string = "\tHello World\n";
-// Print the string with whitespaces
-console.log(`String with whitespaces: ${whitespaces_string}`)
-// Using trim() method for removing whitespaces in the string and assign into a seperate variable because strings are immuteabel, trim method does not change the orignal string 
-let trimmed_whitespaces = whitespaces_string.trim();
-// Print the string without whitespaces
-console.log(`String without whitespaces: ${trimmed_whitespaces}`)
+// // I Used previous question to answer this question. 
+// // In this line I made a variable called whitespaces_string
+// let whitespaces_string : string = "\tHello World\n";
+// // Print the string with whitespaces
+// console.log(`String with whitespaces: ${whitespaces_string}`)
+// // Using trim() method for removing whitespaces in the string and assign into a seperate variable because strings are immuteabel, trim method does not change the orignal string 
+// let trimmed_whitespaces = whitespaces_string.trim();
+// // Print the string without whitespaces
+// console.log(`String without whitespaces: ${trimmed_whitespaces}`)
 
 
 
