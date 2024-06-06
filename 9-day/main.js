092d6fbf0f60edbcb2925670070dce1a763bca5f
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-06-06 12:02:27 +0500

    9th day

---
+++ 9-day/main.js
@@ -0,0 +1,11 @@
+// Question 34: Pizzas: Share your favorite pizzas and express your love for them.
+let flavour_pizza = ["Afghani feast", "Chicken Tandori", "Chicken Tikka"];
+flavour_pizza.forEach((flavour) => {
+    console.log(`I like ${flavour} pizza`);
+});
+// Question 35: Animals: Highlight animals with a common trait.
+let animals = ["Cow", "Dog", "Cat", "Donkey", "Camel"];
+animals.forEach((animal) => {
+    console.log(`${animal} has 4 legs and a tail`);
+});
+export {};
---
+++ 9-day/main.ts
@@ -0,0 +1,17 @@
+// Question 34: Pizzas: Share your favorite pizzas and express your love for them.
+
+let flavour_pizza : string[] = ["Afghani feast", "Chicken Tandori", "Chicken Tikka"];
+
+flavour_pizza.forEach((flavour) => {
+    console.log(`I like ${flavour} pizza`);
+})
+
+
+// Question 35: Animals: Highlight animals with a common trait.
+
+let animals : string[] = ["Cow", "Dog", "Cat", "Donkey", "Camel"];
+
+animals.forEach((animal) => {
+    console.log(`${animal} has 4 legs and a tail`);
+});
+console.log("Any one of them has 4 legs and a tail");
