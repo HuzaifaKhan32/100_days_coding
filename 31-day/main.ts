0bcae17e4b920ed633a8b4fe50f28ce0a06f37a9
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-07-02 21:33:27 +0500

    31-day

---
+++ 31-day/main.ts
@@ -0,0 +1,52 @@
+// Question 103: Write a function that returns a random boolean value, true or false.
+function randomBoolean(): boolean {
+    return Math.random() < 0.5
+}
+
+
+console.log(randomBoolean());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Question 104: Create a function that generates a random hexadecimal color code.
+
+function generateHexaDecimal(): string {
+    let hexaDecimal: string = "#";
+    hexaDecimal += Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0');;
+    return hexaDecimal;
+}
+
+console.log(generateHexaDecimal());
+
+
+
+
+
+
+
+
+// Question 105: Simulate a dice roll using JavaScript and return a random integer between 1 and 6.
+function rollDice() : number{
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+console.log(rollDice());
