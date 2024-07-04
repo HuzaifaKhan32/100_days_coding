7827bc46d7df35181efc178afe1249d4ca3da834
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-07-05 01:59:33 +0500

    34-day

---
+++ 34-day/main.js
@@ -0,0 +1,22 @@
+// Question 112: Create a Map that stores the names of countries as keys and their capitals as values. Add three countries to the Map.
+let countries = new Map();
+countries.set("Pakistan", "Islamabad");
+countries.set("Iran", "Tehran");
+countries.set("USA", "Washington DC");
+countries.set("New Zealand", "Willington");
+console.log(countries);
+// Question 113: Write a function that checks if a Map contains a key for "Canada" and logs the capital if it exists.
+function checkKey(object) {
+    if (object.has("Iran")) {
+        return countries.get("Iran");
+    }
+}
+console.log(checkKey(countries));
+let students = new Map();
+students.set("Huzaifa", 11821);
+students.set("Hamza", 11824);
+students.set("Hashir", 11820);
+students.forEach((id, names) => {
+    console.log(`Student ID: ${id}, Name : ${names}`);
+});
+export {};
