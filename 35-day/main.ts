383fe4c88574d5102bc01289a1bfd386b5ec2244
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-07-07 11:30:16 +0500

    35-day

---
+++ 35-day/main.ts
@@ -0,0 +1,84 @@
+// Question 115: Use a switch statement to log the days of the week based on a number (1-7).
+
+function checkDay(dayNumber: number): void {
+    switch (dayNumber) {
+        case 1:
+            console.log("Monday")
+            break;
+        case 2:
+            console.log("Tuesday")
+            break;
+        case 3:
+            console.log("Wednesday")
+            break;
+        case 4:
+            console.log("Thursday")
+            break;
+        case 5:
+            console.log("Friday")
+            break;
+        case 6:
+            console.log("Saturday")
+            break;
+        case 7:
+            console.log("Sunday")
+            break;
+
+        default:
+            console.log("No Found");
+            break;
+    }
+}
+checkDay(7);
+
+
+
+
+
+
+
+// Question 116: Create a switch case that matches several cases to the same code block, representing seasons.
+
+function checkSeason(month: number): void {
+    switch (month) {
+        case 12:
+        case 1:
+        case 2:
+            console.log("Winter");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            console.log("Spring");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            console.log("Summer");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            console.log("Fall");
+            break;
+        default:
+            console.log("No Found");
+            break;
+    }
+}
+
+checkSeason(12);
+
+// Question 117: Implement a switch statement that evaluates an expression and uses the default case if none of the cases match.
+function evaluatesDefault(age : number): void{
+    switch (age) {
+        case 25:
+            console.log("Adult");
+            break; 
+        default:
+            console.log("Teenager");
+            break;
+    }
+}
+
+evaluatesDefault(17);
